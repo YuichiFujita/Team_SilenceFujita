ace6d884bd639fa4d1cfceea6c30659859726f99
小原立暉 <221134@st.yoshida-g.ac.jp>
2023-03-13 13:56:41 +0900

    プレイヤーの黒煙を追加しました プレイヤーの色が体力により黒ずんでいくようにしました

--- 00_ゲーム制作 ソースファイル/project000 Team SilenceFujita/player.h
+++ 00_ゲーム制作 ソースファイル/project000 Team SilenceFujita/player.h
@@ -26,7 +26,7 @@
 
 #define PLAY_REV_ROT_MIN	(0.75f)		// プレイヤーの向き変更量の減速係数の最小値
 #define PLAY_LIFE			(100)		// プレイヤーの体力
-#define PLAY_DAMAGE			(50)		// プレイヤーのダメージ数
+#define PLAY_DAMAGE			(25)		// プレイヤーのダメージ数
 #define BOOST_WAIT_CNT		(180)		// ブーストの再使用までの時間
 #define WIND_OVERHEAT_CNT	(180)		// ウィンドのオーバーヒートまでの時間
 #define BOMB_WAIT_CNT		(2400)		// ボムの再使用までの時間
@@ -117,10 +117,12 @@ typedef struct
 	PlayerBoost boost;				// ブーストの情報
 	PlayerWind  wind;				// 風の情報
 	PlayerBomb  bomb;				// 爆弾の情報
+	D3DXMATERIAL MatCopy[MAX_MATERIAL];	// マテリアルのコピー
 	int         nLife;				// 体力
 	int         nCounterState;		// 状態管理カウンター
 	int         nShadowID;			// 影のインデックス
 	int			nNumEnterGate;		// 出てくるゲート
+	int			nBrokenCnt;			// 黒煙が出る間隔
 	IconInfo	icon;				// アイコンの情報
 	int			nCameraState;		// 前向きカメラの状態
 	bool		bCameraFirst;		// 一人称カメラの状況
